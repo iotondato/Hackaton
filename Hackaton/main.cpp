b5d9ed80d89cabf4219ffe57a167e00b6bac6aed
Brenno Tondato de Faria <brennotondatodefaria@MacBook-Pro-de-Brenno-Tondato-de-Faria.local>
2014-10-15 21:38:20 -0300

    Initial Commit

---
+++ Hackaton/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hackaton
+//
+//  Created by Brenno Tondato de Faria on 15/10/14.
+//  Copyright (c) 2014 Algoritmos II. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
